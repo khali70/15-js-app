951474460737cf4667ffa40018113d514b448fd2
khali70 <k7alifa.a7med70@gmail.com>
2021-01-19 07:38:02 +0200

    doc the project 1 color flipper

--- 1-color-flipper/app.js
+++ 1-color-flipper/app.js
@@ -2,9 +2,17 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const nhex = document.querySelector(".color");
 const btn = document.getElementById("btn");
 btn.addEventListener("click", backgroud);
+/**
+ * @returns random vlaue from 0 to the prama max
+ * @param {number} max the max random rang value
+ */
 function Ran(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+/**
+ * @description
+ * take random color from the array colors and set the background color to this arr
+ */
 function backgroud() {
   let random = " ";
   random = colors[Ran(4)];
